8c7dffc27bb7bf134614c8810960ff5ed7b0ccdf
EKurganova <EKurganova@sportmaster.ru>
2021-05-25 21:57:04 +0300

    добавила комментарии

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 
 public class Calculator {
+
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        int x = console.nextInt();
+        Scanner console = new Scanner(System.in); // создаем объект и указываем, откуда объект будет брать файлы
+        int x = console.nextInt(); // читаем число и записываем его в переменную
         int y = x * 5 + 3;
         System.out.println(y);
     }
